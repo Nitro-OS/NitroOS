fd81f2e72957643446a530314a9cd59e0c8f3813
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-09 15:57:26 +0500

    feat(scripts): add run uefi

---
+++ releng/run-uefi.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+qemu-system-x86_64 \
+  -enable-kvm \
+  -cpu host \
+  -smp 4 \
+  -m 4096 \
+  -vga virtio \
+  -display gtk,gl=on \
+  -bios /usr/share/ovmf/x64/OVMF.4m.fd \
+  -cdrom ./out/*.iso \
+  -boot d
