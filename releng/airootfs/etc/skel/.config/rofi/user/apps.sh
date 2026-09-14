9b8ed79e1161b173d299c0f1b67d74834d8ea50c
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-14 17:35:36 +0500

    refactor: update application commands in rofi launcher

--- releng/airootfs/etc/skel/.config/rofi/user/apps.sh
+++ releng/airootfs/etc/skel/.config/rofi/user/apps.sh
@@ -10,28 +10,28 @@ list_col=6
 list_row=1
 
 term_cmd='alacritty'
-file_cmd='thunar'
-text_cmd='geany'
+file_cmd='dolphin'
+btop_cmd='alacritty -e btop'
 web_cmd='firefox'
-music_cmd='alacritty -e termusic'
-setting_cmd='xfce4-settings-manager'
+launcher_cmd="$HOME/.config/rofi/launchers/launcher.sh"
+nvim_cmd="alacritty -e nvim $HOME/.config/hypr/"
 
 layout=$(grep 'USE_ICON' "$theme" 2>/dev/null | cut -d'=' -f2)
 
 if [[ "$layout" == 'NO' ]]; then
 	option_1=" Terminal <span weight='light' size='small'><i>($term_cmd)</i></span>"
 	option_2=" Files <span weight='light' size='small'><i>($file_cmd)</i></span>"
-	option_3=" Editor <span weight='light' size='small'><i>($text_cmd)</i></span>"
+	option_3=" btop <span weight='light' size='small'><i>($btop_cmd)</i></span>"
 	option_4=" Browser <span weight='light' size='small'><i>($web_cmd)</i></span>"
-	option_5=" Music <span weight='light' size='small'><i>($music_cmd)</i></span>"
-	option_6=" Settings <span weight='light' size='small'><i>($setting_cmd)</i></span>"
+	option_5=" App Launcher <span weight='light' size='small'><i>($launcher_cmd)</i></span>"
+	option_6=" nvim <span weight='light' size='small'><i>($nvim_cmd)</i></span>"
 else
 	option_1=""
 	option_2=""
-	option_3=""
+	option_3=""
 	option_4=""
-	option_5=""
-	option_6=""
+	option_5=""
+	option_6=""
 fi
 
 rofi_cmd() {
@@ -47,44 +47,34 @@ rofi_cmd() {
 }
 
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5\n$option_6" | rofi_cmd
+	printf '%s\n' \
+		"$option_1" \
+		"$option_2" \
+		"$option_3" \
+		"$option_4" \
+		"$option_5" \
+		"$option_6" |
+		rofi_cmd
 }
 
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		${term_cmd}
-	elif [[ "$1" == '--opt2' ]]; then
-		${file_cmd}
-	elif [[ "$1" == '--opt3' ]]; then
-		${text_cmd}
-	elif [[ "$1" == '--opt4' ]]; then
-		${web_cmd}
-	elif [[ "$1" == '--opt5' ]]; then
-		${music_cmd}
-	elif [[ "$1" == '--opt6' ]]; then
-		${setting_cmd}
-	fi
+	case "$1" in
+		--opt1) eval "$term_cmd" ;;
+		--opt2) eval "$file_cmd" ;;
+		--opt3) eval "$btop_cmd" ;;
+		--opt4) eval "$web_cmd" ;;
+		--opt5) eval "$launcher_cmd" ;;
+		--opt6) eval "$nvim_cmd" ;;
+	esac
 }
 
 chosen="$(run_rofi)"
 
-case ${chosen} in
-	$option_1)
-		run_cmd --opt1
-		;;
-	$option_2)
-		run_cmd --opt2
-		;;
-	$option_3)
-		run_cmd --opt3
-		;;
-	$option_4)
-		run_cmd --opt4
-		;;
-	$option_5)
-		run_cmd --opt5
-		;;
-	$option_6)
-		run_cmd --opt6
-		;;
+case "$chosen" in
+	"$option_1") run_cmd --opt1 ;;
+	"$option_2") run_cmd --opt2 ;;
+	"$option_3") run_cmd --opt3 ;;
+	"$option_4") run_cmd --opt4 ;;
+	"$option_5") run_cmd --opt5 ;;
+	"$option_6") run_cmd --opt6 ;;
 esac
